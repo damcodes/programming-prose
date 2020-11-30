d5c0b75aaff7659a2c9185b08bfcf7db635a6ba9
davidmolina2810 <davidmolina2810@gmail.com>
2020-11-29 21:20:36 -0600

    testing adding image

---
+++ example.py
@@ -0,0 +1,7 @@
+# variable assignment in Python 
+
+num = 10 
+str = "string"
+str2 = 'string'
+list = []
+dict = {}
---
+++ example.rb
@@ -0,0 +1,7 @@
+# variable assignment in Ruby
+
+num = 10 
+str = "string"
+str2 = 'string'
+arr = []
+hash = {}
